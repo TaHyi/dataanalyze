72d9b2b78f255fba0887e9445e8b80bf56edc07f
TaHyi <huyen.hoodlum@gmail.com>
2017-06-07 06:37:26 +0400

    error: not responding interface

--- diplom_huyen_0210/src/ff.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({'foo':list('ABCdkahdgjkfdagjg')})
-print(df)
-print(df.ix[df.index.min(), 'foo'])
-print(df.ix[df.index.max(), 'foo'])
-# print(df.index.min())
-# print(df.index.max())
--- diplom_huyen_0210/src/main.py
+++ diplom_huyen_0210/src/main.py
@@ -6,15 +6,13 @@ except ImportError:
     QString = str
 import sys
 import os
-import ntpath
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, \
     QLineEdit, QDateEdit, QMessageBox, QMenuBar, QAction, QFileDialog, QSpacerItem, QSizePolicy
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5 import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-from twitterscraper import query_tweets
+from matplotlib.figure import Figure
 import pandas as pd
 import string
 import tweepy
@@ -22,45 +20,44 @@ import csv
 import json
 import time
 import datetime
-from io import StringIO
+
 
 class Window(QMainWindow):
     def __init__(self, *args):
         super(Window, self).__init__(*args)
         self.setWindowTitle('Выявление трендов на основе анализа хештегов Твиттер')
-        self.setWindowIcon(QIcon("./icons/twitterlogo.png"))# Set Icon of Window
+        self.setWindowIcon(QIcon("./icons/twitterlogo.png"))  # Set Icon of Window
         self.resize(900, 600)
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         # A statusbar is a widget that is used for displaying status information.
         self.statusBar().showMessage("Готово")  # Statusbar
-        self.initmMain_UI()
-        self.initMenu()
+        self.init_menu()
 
-    def initMenu(self):
+    def init_menu(self):
         # Create Menubar
         #  Add "Open File" Action
-        openAction = QAction(QIcon('./icons/openfile.jpg'), '&Открыть файл', self)
-        openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Открыть приложение')
-        # Connect object "openAction" to function "file_open"
-        openAction.triggered.connect(self.get_data_from_file)
+        open_action = QAction(QIcon('./icons/openfile.jpg'), '&Открыть файл', self)
+        open_action.setShortcut('Ctrl+O')
+        open_action.setStatusTip('Открыть приложение')
+        # Connect object "open_action" to function "file_open"
+        open_action.triggered.connect(self.get_data_from_file)
         #  Add "Exit" Action
-        exitAction = QAction(QIcon('./icons/exit.png'), '&Закрыть приложение', self)
-        exitAction.setShortcut('Ctrl+W')
-        exitAction.setStatusTip('Закрыть приложение')
-        # Connect object "exitAction" to function "closeEvent"
+        exit_action = QAction(QIcon('./icons/exit.png'), '&Закрыть приложение', self)
+        exit_action.setShortcut('Ctrl+W')
+        exit_action.setStatusTip('Закрыть приложение')
+        exit_action.triggered.connect(self.close)
+        # Connect object "exit_action" to function "closeEvent"
         # Create status bar and add actions to menubar
         self.statusBar()
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&Файл')
-        fileMenu.addAction(openAction)
-        fileMenu.addAction(exitAction)
+        file_menu = menubar.addMenu('&Файл')
+        file_menu.addAction(open_action)
+        file_menu.addAction(exit_action)
 
     # Design Gui of application
-    def initmMain_UI(self):
         # Create a figure instance to plot on
-        self.figure = plt.figure()
+        self.figure = Figure()
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
@@ -79,10 +76,9 @@ class Window(QMainWindow):
         # Add Date Edit into Gui
         self.start_time_input = QDateEdit()
         self.start_time_input.setCalendarPopup(True)
-        self.start_time_input.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.start_time_input.setDateTime(QtCore.QDateTime(QtCore.QDate(2017, 6, 5), QtCore.QTime(1, 0, 0)))
         self.end_time_input = QDateEdit()
         self.end_time_input.setCalendarPopup(True)
-
         self.end_time_input.setDateTime(QtCore.QDateTime.currentDateTime())
         # Buttion Get Informations
         self.getInforBtn = QPushButton('Получить информацию')
@@ -106,53 +102,54 @@ class Window(QMainWindow):
         layout123.addLayout(layout3)
         # Layout 3Cot 3 colums
         layout3Cot = QHBoxLayout()
-        label= QtWidgets.QLabel()
+        label = QtWidgets.QLabel()
         layout3Cot.addWidget(label)
         verticalSpacer = QtWidgets.QSpacerItem(200, 100, QtWidgets.QSizePolicy.Maximum, QSizePolicy.Expanding)
         layout3Cot.addItem(verticalSpacer)
         layout3Cot.addLayout(layout123)
         layout3Cot.addWidget(self.getInforBtn)
-
+        self.result = QLabel("")
+        self.time = QLabel("")
         # set the layout
         layoutGraph = QVBoxLayout()
         # layoutGraph.addWidget(self.button)
         layoutGraph.addLayout(layout3Cot)
         layoutGraph.addWidget(self.canvas)
+        layoutGraph.addWidget(self.time)
+        layoutGraph.addWidget(self.result)
+        self.time.setFont(QtGui.QFont("Times", 10, weight=QtGui.QFont.Normal))
+        self.result.setFont(QtGui.QFont("Times",12, weight=QtGui.QFont.DemiBold))
         layoutGraph.addWidget(self.toolbar)
         self.central_widget.setLayout(layoutGraph)
         # connect slot, signal
         self.getInforBtn.clicked.connect(self.getInformation)
-
         self.df = pd.DataFrame()
-        self.time_list =[]
-
+        self.time_list = []
         self.show()
 
     # Work with file
-    def get_data_from_file(self):
-        try:
-            file_path, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "",
-                                                       "All Files (*);;CSV файлы (*.csv)")
-            if file_path:
-                print (file_path)
-                file_name = os.path.basename(file_path)
-                self.df = pd.read_csv(file_name)
-                self.time_list = self.df.created_at.unique().tolist()
-                self.analyze_data()
-                self.plot(file_name[:-15])
-                self.statusBar().showMessage("Визуализация данных файла о #%s" % file_name[:-15], 2000)
 
+    def get_data_from_file(self):
+        print("load file")
+        try:
+            file_choices = "CSV (*.csv)|*.csv"
+            path = (QFileDialog.getOpenFileName(self, 'Load test file', '', file_choices))
+            self.df = pd.read_csv(path[0], ',')
+            self.time_list = self.df.created_at.unique().tolist()
+            self.analyze_data()
+            file_name = os.path.basename(path[0])
+            self.plot(file_name[:-15])
+            self.statusBar().showMessage('Визуализация данных файла о #%s' % file_name[:-15], 2000)
         except  Exception:
-            self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0])
+            self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
 
     def analyze_data(self):
         # create new dataFrame. Create into it 1 column "Time", pull list "time_list" into its column
         # replace missing values with 0
         self.df_filter = pd.DataFrame()
         self.df_filter['DateTime'] = self.time_list
-        self.df_filter = pd.DataFrame()
-        self.df_filter['DateTime'] = self.time_list
-
+        date0 = datetime.datetime.strptime(self.time_list[0], "%Y-%m-%d %H:%M:%S")
+        date1 = datetime.datetime.strptime(self.time_list[-1], "%Y-%m-%d %H:%M:%S")
         # create a serie form to count value of
         series_count = self.df['created_at'].value_counts()
         # # bi xao tron vi tri
@@ -160,20 +157,44 @@ class Window(QMainWindow):
         self.df_filter['DateTime'] = pd.to_datetime(self.df['created_at'])
         self.df_filter.index = self.df_filter['DateTime']
         self.ts = self.df_filter['Count']
-        self.df_grouped = self.ts.resample('10Min').sum()
-        # self.df_grouped = self.ts.resample('60Min').sum()
-        self.df_grouped = self.df_grouped.fillna(0)
-        self.moving_avg = self.df_grouped.rolling(window=6).mean()
-        print("blue")
-        self.df_grouped.to_csv('grouped.csv')
-        print(self.df_grouped.head(12))
-        print(self.df_grouped.ix[self.df_grouped.index.min(), 'DateTime'])
-        print(self.df_grouped.ix[self.df_grouped.index.max(), 'DateTime'])
-        # print("red")
-        # print(self.moving_avg.head(12))
+        if ((date1 - date0).days >= 100):
+            self.df_grouped = self.ts.resample('60Min').sum()
+            self.df_grouped = self.df_grouped.fillna(0)
+            print(self.df_grouped.isnull().sum())
+            self.moving_avg = self.df_grouped.rolling(window=24).mean()
+            y_t0 = self.moving_avg.ix[self.moving_avg.index.min(), 'DateTime']
+            y_tk = self.moving_avg.ix[self.moving_avg.index.max(), 'DateTime']
+            print("Первая точка функции скользящего среднего y_t0= ", end='')
+            print(y_t0)
+            print("Последняя точка функции скользящего среднего y_tk= ", end='')
+            print(y_tk)
+            if (y_t0 > y_tk):
+                self.time.setText("Выявление тренды в промежутке времени ["+ self.time_list[0] + ";" + self.time_list[-1]+ "]")
+                print("Долгосрочный, тренд нисходящий")
+                self.result.setText("Долгосрочный, тренд нисходящий")
+            else:
+                self.time.setText("Выявление тренды в промежутке времени ["+ self.time_list[0] + ";" + self.time_list[-1]+ "]")
+                print("Долгосрочный, тренд восходящий")
+                self.result.setText("Долгосрочный, тренд восходящий")
+        else:
+            self.df_grouped = self.ts.resample('30Min').sum()
+            self.df_grouped = self.df_grouped.fillna(0)
+            self.moving_avg = self.df_grouped.rolling(window=2).mean()
+            x_tk = self.df_grouped.ix[self.df_grouped.index.max(), 'DateTime']
+            y_tk = self.moving_avg.ix[self.moving_avg.index.max(), 'DateTime']
+            print("Последняя точка функции скользящего среднего y_tk= ", end='')
+            print(y_tk)
+            print("Последняя точка на реальном x_tk= ", end='')
+            print(x_tk)
+            if (y_tk > x_tk):
+                self.time.setText("Выявление тренды в промежутке времени ["+ self.time_list[0] + ";" + self.time_list[-1]+ "]")
+                print("Краткосрочный, тренд нисходящий")
+                self.result.setText("Краткосрочный, тренд нисходящий")
+            else:
+                self.time.setText("Выявление тренды в промежутке времени ["+ self.time_list[0] + ";" + self.time_list[-1]+ "]")
+                print("Краткосрочный, тренд восходящий")
+                self.result.setText("Краткосрочный, тренд восходящий")
 
-
-        # self.moving_avg = self.df_grouped.rolling(window=12).mean()
     def plot(self, hashtag):
         try:
             # instead of ax.hold(False)
@@ -184,17 +205,10 @@ class Window(QMainWindow):
             ax.set_title('Визуализация тренды хештеги #' + hashtag)
             ax.set_xlabel('Время')
             ax.set_ylabel('Количество')
-            plt.xticks(rotation=15)
+            # ax.xticks(rotation=15)
             ax.plot(self.df_grouped, color='blue', marker='o', markersize=3, label='Количество хештеги линии')
             ax.plot(self.moving_avg, color='red', label='Тренды хештеги линии')
-            # print("blue")
-            # print(self.df_grouped.head())
-            # print("red")
-            # print(self.moving_avg.head())
-
-            # plt.gcf().autofmt_xdate()
             ax.legend()
-
             # refresh canvas
             self.canvas.draw()
             self.statusBar().showMessage("Готово")
@@ -209,6 +223,7 @@ class Window(QMainWindow):
             event.accept()
         else:
             event.ignore()
+
     def getInformation(self):
         try:
             hashtag_input = self.hashtag_input.text()
@@ -233,17 +248,17 @@ class Window(QMainWindow):
                 API_KEY = "vuuHtQehJVLkfQEL7pUxOX4yW"
                 API_SECRET = "XPfT8jIJeSvoghsIjcDpJXYKaKSlS9KGyhSOlksDL3EdirsiRD"
                 auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
-                api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+                api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
                 if (not api):
                     self.statusBar().showMessage("Не удается аутентифицировать")
-                    print ("Не удается аутентифицировать")
+                    print("Не удается аутентифицировать")
                     sys.exit(-1)
                 searchQuery = '#' + hashtag_input + ' since:' + start_day + ' until:' + end_day
                 # this is what we're searching for hashtag_input since:start_day until:end_day
-                maxTweets = 10000000 # Some arbitrary large number
+                maxTweets = 10000000  # Some arbitrary large number
                 tweetsPerQry = 100  # this is the max the API permits
-                fName = hashtag_input+start_day[5:]+'_'+ end_day[5:]+'.csv'
-                fNamePath = "./"+ fName# We'll store the tweets in a text file.
+                fName = hashtag_input + start_day[5:] + '_' + end_day[5:] + '.csv'
+                fNamePath = "./" + fName  # We'll store the tweets in a text file.
                 raw_data = []
                 # If results from a specific ID onwards are reqd, set since_id to that ID.
                 # else default to no lower limit, go as far back as API allows
@@ -254,7 +269,7 @@ class Window(QMainWindow):
                 tweetCount = 0
                 self.statusBar().showMessage("Загрузка максимум {0} твитов".format(maxTweets))
                 print("Загрузка максимум {0} твитов".format(maxTweets))
-                with open(fNamePath,'w',encoding='utf-8') as f:
+                with open(fNamePath, 'w', encoding='utf-8') as f:
                     writer = csv.writer(f)
                     writer.writerow(["id", "created_at", "text"])
                     while tweetCount < maxTweets:
@@ -290,19 +305,21 @@ class Window(QMainWindow):
                             self.statusBar().showMessage("some error : " + str(e))
                             print("some error : " + str(e))
                             break
-                self.df = pd.DataFrame()
-                self.df['id'] =list(map(lambda tweet: tweet['id'], raw_data))
+                # self.df = pd.DataFrame()
+                self.df['id'] = list(map(lambda tweet: tweet['id'], raw_data))
 
                 self.df['created_at'] = list(map(lambda tweet: time.strftime('%Y-%m-%d %H:%M:%S',
-                                                                            time.strptime(tweet['created_at'],
-                                                                                          '%a %b %d %H:%M:%S +0000 %Y')),
-                                                raw_data))
+                                                                             time.strptime(tweet['created_at'],
+                                                                                           '%a %b %d %H:%M:%S +0000 %Y')),
+                                                 raw_data))
                 self.df['text'] = list(map(lambda tweet: tweet['text'], raw_data))
                 self.time_list = self.df.created_at.unique().tolist()
                 self.analyze_data()
                 self.plot(fName[:-15])
         except  Exception:
             self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = Window()
